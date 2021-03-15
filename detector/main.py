d72e2c2d3d334adea33660ff62dfe251386fd3af
Celalettin Yılmaz <celalettinyilmaz@aof.anadolu.edu.tr>
2021-03-15 12:45:22 +0300

    Update main.py

--- detector/main.py
+++ detector/main.py
@@ -13,7 +13,7 @@ def scrape_data(username):
       
     s = BeautifulSoup(r.text, "html.parser") 
       
-    meta = s.find("meta")
+    meta = s.find("meta", property="og:description")
     print(meta)
     
     return meta.attrs['content'] 
