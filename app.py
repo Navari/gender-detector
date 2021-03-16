1f1855a1a923cac819f51bfcf02a23ed9c3e6ec8
Celalettin Yılmaz <celalettinyilmaz@aof.anadolu.edu.tr>
2021-03-16 11:55:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from detector import main
 app = Flask(__name__, static_url_path='/static')
 
 
-@app.route('/<username>')
-def hello_world(username):
-    if username != 'favicon.ico':
-        response = main.run(username)
-        return jsonify(response)
+@app.route('/')
+def hello_world():
+    response = main.run(request.form.get('image_url'))
+    return jsonify(response)
 
 
 if __name__ == '__main__':
